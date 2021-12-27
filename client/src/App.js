93513b25e78e604dbd31217a650e7fe5271ffd24
Ali Chaabane <milik9898>
2021-12-27 22:16:31 +0100

    implement add and get food api & start frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState, useEffect } from "react";
+import Axios from "axios";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
+  const [foodName, setFoodName] = useState('');
+  const [days, setDays] = useState(0);
+  const [isInserted, setIsInserted] = useState(false);
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect( () => {
+    Axios.get('http://localhost:3002/getFoods').then((response) => {
+      setFoodList(response.data);
+    });
+  }, [])
+ let addToList = () => {
+    Axios.post('http://localhost:3002/addFood', {foodName, days}).then(  (res) =>  {
+      console.log('result : ', res.status);
+      if (res.status === 200) {
+        setIsInserted(true);
+        setTimeout(() => {
+          setIsInserted(false);
+      }, 2000);
+      }
+    }
+    ).catch (err => {
+      console.error('error : ', err);
+      setIsInserted(false);
+    }) ;
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1> CRUD App with MERN : </h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formFoodName">
+          <Form.Label>Food Name : </Form.Label>
+          <Form.Control type="email" onChange={(event) => {
+            setFoodName(event.target.value);
+          }} placeholder="Food Name" />
+     
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formDays">
+          <Form.Label>Days since you Ate it : </Form.Label>
+          <Form.Control type="number" min="0" onChange={(event) => {
+            setDays(event.target.value);
+          }} placeholder="Days since you Ate it" />
+        </Form.Group>
+        <Button onClick={addToList}  variant="success">
+          Add to List
+        </Button>
+        <Alert  className="mt-4" show={isInserted === true}  variant="success">check it out!</Alert> 
+
+        <div className="mt-3">
+          <h2>Food List : </h2>
+            {
+              foodList.map((val, key) => {
+                return ( <div key={key}>
+                  <span>
+                    {val.foodName}
+                  </span>
+                    <span className="mx-2">
+                    - Days : {val.daysSinceIAte}
+                  </span>
+                </div>
+              );
+              })
+            }
+        </div>
+      </Form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,26 +3,37 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const FoodModel = require("./models/Food");
-
+const cors = require("cors");
 dotenv.config();
 app.use(express.json());
+app.use(cors());
+mongoose.connect(
+  "mongodb+srv://alichaabane98:root@crud.kwz6w.mongodb.net/food?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
-mongoose.connect('mongodb+srv://alichaabane98:root@crud.kwz6w.mongodb.net/food?retryWrites=true&w=majority', { useNewUrlParser: true })
-
-
-app.get('/', async (req, res) => {
- const food = new FoodModel({foodName: 'Apple', daysSinceIAte: 3})
-    try {
-        await food.save();
-        res.send("inserted data");
-    } catch(err) {
-        console.error(err);
+app.get("/getFoods", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
     }
+    res.send(result);
+  });
+});
+app.post("/addFood", async (req, res) => {
+  const foodName = req.body.foodName;
+  const days = req.body.days;
 
+  const food = new FoodModel({ foodName: foodName, daysSinceIAte: days });
+  try {
+    await food.save();
+    res.send("inserted data");
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log('Your server is running on port :',port); // 3001
-
+  console.log("Your server is running on port :", port); // 3001
 });
